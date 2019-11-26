0f4b61b57ba310d7425c5adcbdfb48d22b37672a
Lydia <lydiaquinnitni@gmail.com>
2019-11-26 19:49:44 +0000

    Change background and photographer on click

--- js/background.js
+++ js/background.js
@@ -12,20 +12,23 @@ $( document ).ready(function(){
   $('h1').on('click', function () {
 
     // 1. log to the console that you have clicked the button
-
+    // writes to the log 'I got clicked!'
+    console.log('I got clicked!')
+    
     // this selects a randon background from above and stores it as the variable 'randomBackground'
     var randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
     // this creates a string that css will understand to be a background url
     var imageUrl = "url('./images/" + randomBackground + ".jpg')"
     
-    
     // 2. using jquery, change the image url of the body background to the new one saved above
+    $('body').css('background', imageUrl)
 
     // EXTENSION TASK!
     // 3. Change the text of the 'span' to be that of the photographer
     //   (hint, you should re-use randomBackgroun)
     // Double points if you replace '-' between their names with a space
-
+    var artist = randomBackground.replace(/-/g, ' ')
+    $('.photographer').text(artist)
     
   }) 
 }) // here endeth the function
